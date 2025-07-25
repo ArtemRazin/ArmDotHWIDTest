5a1ec87ca64412d9c65686ac69cd90e95dd205fd
Artem Razin <razin@softanics.com>
2025-07-25 16:14:24 +0300

    Added files

---
+++ Program.cs
@@ -0,0 +1,3 @@
+﻿var id = ArmDot.Client.Api.GetCurrentMachineHardwareId(ArmDot.Client.Api.Hardware.CPU | ArmDot.Client.Api.Hardware.Motherboard | ArmDot.Client.Api.Hardware.HDD);
+Console.WriteLine($"Your Activation Key Is: {id}");
+
